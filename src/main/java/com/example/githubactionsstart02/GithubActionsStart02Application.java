7a6eb4fc187537b9d357707e7f0952c252fb8036
KEATLL <thom4a14@stud.kea.dk>
2023-11-14 08:37:41 +0100

    Initial commit

---
+++ src/main/java/com/example/githubactionsstart02/GithubActionsStart02Application.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsstart02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsStart02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsStart02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubactionsstart02/GithubActionsStart02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsstart02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsStart02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
